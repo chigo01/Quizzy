f3d9343d62d0e0fae241e0971a8723534bed5e7e
chigo01 <78267322+chigo01@users.noreply.github.com>
2022-04-26 04:26:18 +0100

    Update main.dart

--- quizzy/lib/main.dart
+++ quizzy/lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quizzy/screens/game_ready_screen.dart';
 import 'package:quizzy/screens/game_screen.dart';
 import 'package:quizzy/screens/home_screen.dart';
 import 'package:quizzy/screens/result.dart';
 
-void main() => runApp(const Quizzy());
+void main() {
+  runApp(const Quizzy());
+  //WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.landscapeLeft,
+  ]);
+}
 
 class Quizzy extends StatelessWidget {
   const Quizzy({Key? key}) : super(key: key);
